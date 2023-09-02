b84ed0530ac402f11621309f572cc2636fcb54df
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-09-02 20:35:21 +0600

    updae2

--- pages/_app.js
+++ pages/_app.js
@@ -2,17 +2,18 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
+import img from "../public/homebg.jpg"
 import "../public/css/style.css"
 import Cards from "../pages/component/Cards"
+
 export default function Home() {
   return (
     <div className="home">
       <header className='header'>
-            <h1>MAEMAN</h1>
-            <p> WHAT'S INCLUDED</p>
-        </header>
-        <Cards/>
- 
+        <h1>MAEMAN</h1>
+        <p>WHAT'S INCLUDED</p>
+      </header>
+      <Cards/>
     </div>
   )
 }
