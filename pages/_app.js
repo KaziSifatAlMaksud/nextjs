3915a2203cf52876319187bfd6c861b320a5fd50
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2023-09-02 21:00:32 +0600

    dfafj

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import { config } from '@fortawesome/fontawesome-svg-core'
 import '../node_modules/@fortawesome/fontawesome-svg-core/styles.css'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
-import homebg from '../homebg.jpg';
 import "../public/css/style.css"
 import Cards from "../pages/component/Cards"
 
