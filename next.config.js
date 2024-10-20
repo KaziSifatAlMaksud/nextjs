072a524c5879e8f3d5249183add2c22ccb25d3fb
Kazi Sifat Al Makusd <65750595+KaziSifatAlMaksud@users.noreply.github.com>
2024-10-20 22:31:50 +0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        domains: ["kazisifat.lgorithmbd.com"],
+        unoptimized: true,
+    },
+    output: "export",
+}
 
 module.exports = nextConfig
